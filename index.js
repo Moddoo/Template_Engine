ef7036017067718610ab16e8e6d7546bd4f42b1b
Moddoo <zoddoo@yahoo.com>
2020-03-09 01:57:20 -0500

    not yet

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 const inquirer = require('inquirer');
 const fs       = require('fs');
 const util     = require('util');
-const axios    = require('axios');
 
-const Manager = require('./lib/Manager');
+const Manager  = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
+const Intern   = require('./lib/Intern');
 
-let output = [];
+const readFileAsync  = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 let questions = [
   {
     type: 'input',
     name: 'name',
-    message: "What's your full name?"
+    message: "What's The full name?"
   },
   {
     type: 'input',
     name: 'id',
-    message: "What's your ID number?"
+    message: "What's The ID number?"
   },
   {
     type: 'input',
     name: 'email',
-    message: "What's your Email address?"
+    message: "What's The Email address?"
   },
   {
       type: 'list',
       name: 'title',
-      message: "What's your role in the team?",
+      message: "What's The role in the team?",
       choices: ['Manager', 'Engineer', 'Intern']
   },
   {
     type: 'input',
     name: 'office',
-    message: "What's your Office number?",
+    message: "What's The Office number?",
     when: ans => ans.title === 'Manager'
   },
   {
     type: 'input',
     name: 'github',
-    message: "What's your Github Username?",
+    message: "What's The Github Username?",
     when: ans => ans.title === 'Engineer'
   },
   {
     type: 'input',
     name: 'school',
-    message: "What's your School?",
+    message: "What's The name of the School?",
     when: ans => ans.title === 'Intern'
   },
   {
@@ -59,13 +59,38 @@ let questions = [
 
 async function init() {
     try{
-         const x = await inquirer.prompt(questions);
-         output.push(x)
-         if (x.askAgain)  return init();
+         const answers = await inquirer.prompt(questions);
+      
+         switch(answers.title) {
+            case 'Manager': html('Manager', answers);
+            break;
+            case 'Engineer': html('Engineer', answers);
+            break;
+            case 'Intern': html('Intern', answers);
+            break;    
+         }
+         if (answers.askAgain)  {
+           console.log('\n')
+           return init()
+          };
     }
     catch (err){
        console.log(err)
     }
-    console.log('Your Team Info:', output);
 }
 init()
+
+async function html(member, ans) {
+   try { 
+     const template = await readFileAsync(`${__dirname}/output/template/${member}.html`, 'utf8');
+     const team   = (await readFileAsync(`./output/team.html`, 'utf8')).split('<pre></pre>');
+     const x = eval("`" + template + "`");
+     const y = '<pre></pre>';
+     const output   = await writeFileAsync('./output/team.html', `${team[0]} ${x} \n  ${y} ${team[1]}`, 'utf8');
+     return output
+    } 
+    catch(err) {
+      console.log(err)
+    }
+}
+// html('Manager')
