06df8c67bb83d1ba39183a5b134005c378c530cd
Moddoo <zoddoo@yahoo.com>
2020-03-11 00:38:28 -0500

    done

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Template_Engine
+
+## Description
+
+The Idea of this project is creating afile holding the data of every team(Manager, Engineer, Intern); in asimple and easy way...
+
+## Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Tests](#tests)
+* [Question](#question)
+                 
+## Installation
+
+TextEditor Like Vscode, Node.js ...
+                
+## Usage
+ 1. Go to terminal and type npm init -y
+ 1. then npm i to install the required modules (dependencies)
+ 1. then node index.js to run the project
+ 1. answer the prompts of questions in the terminal and once you are done anew file will be created holding the data of your team in awell styled page.
+
+## Credits
+ 
+I did this project by myself.
+
+## License
+![badge](https://img.shields.io/badge/Moddoo-ahmed-teal)
+
+MIT license 
+                
+## Tests
+
+![badge](https://img.shields.io/badge/License-ahmed-yellowgreen)
+![badge](https://img.shields.io/badge/dependencies-upto_date-hotpink)
+![badge](https://img.shields.io/badge/Version-v1.0.0-blue)
+![gif](${ans.tests})
+                
+## Question
+
+![github photo](${response.data.avatar_url})                  
+${ans.question}
--- index.js
+++ index.js
@@ -8,47 +8,53 @@ const Intern   = require('./lib/Intern');
 
 const readFileAsync  = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
-let   file  = 1;
+
 
 let questions = [
   {
     type: 'input',
     name: 'name',
-    message: "What's The full name?"
+    message: "What's The full name?",
+    validate: validateName
   },
   {
     type: 'input',
     name: 'id',
-    message: "What's The ID number?"
+    message: "What's The ID number?", 
+    validate: validateNum
   },
   {
     type: 'input',
     name: 'email',
-    message: "What's The Email address?"
+    message: "What's The Email address?",
+    validate: validateEmail
   },
   {
-      type: 'list',
-      name: 'title',
-      message: "What's The role in the team?",
-      choices: ['Manager', 'Engineer', 'Intern']
+    type: 'list',
+    name: 'title',
+    message: "What's The role in the team?",
+    choices: ['Manager', 'Engineer', 'Intern']
   },
   {
     type: 'input',
     name: 'office',
     message: "What's The Office number?",
-    when: ans => ans.title === 'Manager'
+    when: ans => ans.title === 'Manager',
+    validate: validateNum
   },
   {
     type: 'input',
     name: 'github',
     message: "What's The Github Username?",
-    when: ans => ans.title === 'Engineer'
+    when: ans => ans.title === 'Engineer',
+    validate: validateTxt
   },
   {
     type: 'input',
     name: 'school',
     message: "What's The name of the School?",
-    when: ans => ans.title === 'Intern'
+    when: ans => ans.title === 'Intern',
+    validate: validateName
   },
   {
     type: 'confirm',
@@ -62,29 +68,55 @@ let questions = [
     message: 'Want to Add aNew Team??',
     default: true,
     when: ans => ans.askAgain === false
-  }
+  },
 ];
+
+function validateNum(input) {
+  // /^\d+$/.test(input) ? true : console.log('\nINVALID: You should put anumber')
+  if(!/^\d+$/.test(input)) console.log('\n Err: You should put anumber');
+  else return true;
+}
+function validateTxt(input) {
+  if(!/^[\w]+$/.test(input)) console.log('\n Err: No special characters');
+  else return true;
+}
+function validateName(input) {
+  if(!/^[A-z ]+$/.test(input)) console.log('\n Err: Letters Only');
+  else return true;
+}
+function validateEmail(input) {
+  if(!/^[^@]+@[\w]+\.[A-z]+$/.test(input)) console.log('\n Err: Invalid Email');
+  else return true;
+}
+
+let file = 1;
+let z = false;
 
 async function init() {
     try{
-         let baseTeamFile = await readFileAsync(`./output/team.html`, 'utf8');
-         let newTeamFile  = await writeFileAsync(`team${file}.html`, baseTeamFile, 'utf8');
+      if(!z) {
+        let baseTeamFile = await readFileAsync(`./output/team.html`, 'utf8');
+        let newTeamFile  = await writeFileAsync(`team${file}.html`, baseTeamFile, 'utf8');
+        z = true;
+      }
+
+      const answers = await inquirer.prompt(questions);
         
-        const answers = await inquirer.prompt(questions);
          switch(answers.title) {
-            case 'Manager': html('Manager', answers);
+            case 'Manager': await html('Manager', answers);
             break;
-            case 'Engineer': html('Engineer', answers);
+            case 'Engineer': await html('Engineer', answers);
             break;
-            case 'Intern': html('Intern', answers);
+            case 'Intern': await html('Intern', answers);
             break;    
          }
          if(answers.askAgain)  {
-           console.log('\n')
+           console.log('\n');
            return init()
           }
           if(answers.newTeam) {
            console.log('\n');
+           z = false;
            file++;
            return init();
         }    
